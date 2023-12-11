7286f78eae53cb83d3c1109ae04d49ebd58ca2bd
yolo <gordeevadashka@mail.ru>
2023-12-11 20:14:14 +0700

    Add file

---
+++ data.py
@@ -0,0 +1,17 @@
+headers = {
+    "Content-Type": "application/json"
+}
+
+user_body = {
+    "firstName": "Анатолий",
+    "phone": "+79995553322",
+    "address": "г. Москва, ул. Пушкина, д. 10"
+}
+
+product_kit = {
+       "name": "Мой набор",
+       "card": {
+           "id": 1,
+           "name": "Под ситуацию"
+       }
+}
